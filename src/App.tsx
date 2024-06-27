690057b052f2d0dcf0b059bf55b41c49bfe5cd2d
vlad <supervl2011@yandex.ru>
2024-06-27 12:45:02 +0300

    add comment 4

--- src/App.tsx
+++ src/App.tsx
@@ -193,7 +193,7 @@ function App() {
         <StyledButton
           disabled={
             Boolean(props.errors.password) || Boolean(props.errors.repeatPassword) || !props.dirty
-            //something new 3 car
+            //something new 4 car
           }
           onClick={() => setStep('Review')}
         >
